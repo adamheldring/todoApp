ad613c21a01b592536ef49018e6e9b261043698e
A-Game <javascript@adamheldring.com>
2018-10-25 15:02:58 +0200

    Updates

---
+++ precache-manifest.e03b7524b22c873f4861e8a50391a0b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92bbb379fe3e87e2f8fd",
+    "url": "/todoApp/static/js/runtime~main.92bbb379.js"
+  },
+  {
+    "revision": "ee510b193633dab4b813",
+    "url": "/todoApp/static/js/main.ee510b19.chunk.js"
+  },
+  {
+    "revision": "90ca9899eb1533c4a8a8",
+    "url": "/todoApp/static/js/1.90ca9899.chunk.js"
+  },
+  {
+    "revision": "ee510b193633dab4b813",
+    "url": "/todoApp/static/css/main.095cf694.chunk.css"
+  },
+  {
+    "revision": "e569ae4bae840c17797b82bc7f3a34d7",
+    "url": "/todoApp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/todoApp/precache-manifest.e03b7524b22c873f4861e8a50391a0b6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/todoApp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
